4b26db1f0f8cc0cdabe18249225847a1f705f60e
Hikamizan <hikamizan771@gmail.com>
2022-05-15 19:47:48 +0700

    Add exercise.js object file

---
+++ exercise.js
@@ -0,0 +1,18 @@
+// examples
+
+const exercises = [
+    {
+        id: 1,
+        name: "push up",
+        caloriesBurn: 49,
+        amount: 5 // in minutes
+    },
+    {
+        id: 1,
+        name: "pull up",
+        caloriesBurn: 55,
+        amount: 5 // in minutes
+    },
+]
+
+module.exports = exercises;
