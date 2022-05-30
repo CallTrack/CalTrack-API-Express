4f81d11c16e90433419f0bb29beb37046e456016
Muhammad Diffa Ananda Lukman <diffa.ananda.10@gmail.com>
2022-05-30 19:00:20 +0700

    nambah cors pada server

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors({origin: '*'}));
 
 app.use(express.static(path.join(__dirname, 'public')))
 
